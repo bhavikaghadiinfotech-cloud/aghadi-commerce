a955666f418a4574a7a49512ccdb7eaf9b52d82b
bhavikaghadiinfotech-cloud <bhavik.aghadiinfotech@gmail.com>
2026-01-07 17:02:30 +0530

    frontend functionality code

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,11 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <App />
     </Provider>
   </React.StrictMode>
 );
